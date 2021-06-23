1e1f120e189d47d252af75ce2f286d478be7e095
Thuy Nguyen <nhuythuy@gmail.com>
2021-06-23 21:40:31 +0200

    added sensor + actuator signals

--- solarpower1.ino
+++ solarpower1.ino
@@ -1,5 +1,6 @@
 
 #include "sensors.h"
+#include "actuators.h"
 #include "datetime.h"
 #include "blynk.h"
 
@@ -48,18 +49,46 @@ void setup()
   Serial.begin(115200);
   delay(100);
 
+
   setupSensors();
   delay(100);
 
+  setupActuators();
+  delay(100);
 
   BlynkEdgent.begin();
-
   timer.setInterval(2000L, blynkTimerEvent);
 
   setupDateTime();
+
+  autoLoadPower = (boolean)readState(ADDRESS_AUTO_LOAD_POWER);
+  manualLoadPowerOn = (boolean)readState(ADDRESS_MANUAL_LOAD_POWER_ON);
 }
 
+unsigned long previousMillis = millis();
+unsigned long currentMillis = millis();
+long diffMillis = 0;
+
 void loop() {
+  currentMillis = millis();
+  runtimeMinutes = currentMillis / 60000;
+  diffMillis = currentMillis - previousMillis;
+  if(abs(diffMillis) > 2000){  // sampling sensors every 2 sec
+    previousMillis = currentMillis;             // save the last time  
+
+    yield();
+    getServerTime();
+
+    yield();
+    updateBattVolt();
+    yield();
+    updateTempHumid();
+    yield();
+    updateActuators();
+  }
+  
   BlynkEdgent.run();
 
+  flipLed();
+  delay(100);
 }
